c23006a2238ee698dc165484982cb5bad48f9462
Nayanz <nayana.raj@cet.ac.in>
2020-08-04 17:14:48 -0400

    Assinment solution5

--- module4/Script.js
+++ module4/Script.js
@@ -1,6 +1,6 @@
 (function(){
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Nayana", "Shanu", "John", "Lucy", "Jeena", "Jason", "Thomas", "Jacobe", "Marry", "Arun", "Larry", "Jin"];
 	for (var i = 0; i < names.length; i++) {
 		var firstLetter = names[i].charAt(0).toLowerCase();
 		 if (firstLetter === 'j') {
